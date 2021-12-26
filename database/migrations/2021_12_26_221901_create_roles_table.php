19f57903f30428af7174c490c87d6faa76cb9a71
maksiz32 <maks-manzulin@mail.ru>
2021-12-27 01:23:29 +0300

    10 Миграция добавления таблицы ролей

---
+++ database/migrations/2021_12_26_221901_create_roles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('role', 25);
+            $table->string('name', 255)->comment('Название роли для представления');
+            $table->timestamp();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}

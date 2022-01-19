e085637ee3a4af96a78c043132c57497deb6513f
kaminyv <kaminyv@yandex.ru>
2022-01-19 23:18:45 +0400

    Посев для пользователей, мастеров и услуг

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,11 +15,14 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+            'name' => $this->faker->firstName,
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'is_master' => false,
             'remember_token' => Str::random(10),
+            'avatar' => $this->faker->image('public/storage',640,480, null, false),
+            'phone' => $this->faker->numerify('79#########'),
         ];
     }
 
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,8 +3,11 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+use Faker;
+
 
 class UserSeeder extends Seeder
 {
@@ -15,11 +18,19 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-                                       'name' => 'John Smith',
-                                       'email' => 'maks-manzulin@mail.ru',
-                                       'is_master' => 1,
-                                       'password' => Hash::make('123456Qw'),
-                                   ]);
+        $faker = Faker\Factory::create('ru_RU');
+        DB::table('users')
+            ->insert([
+                'name' => 'Анна',
+                'email' => 'anna@mail.ru',
+                'is_master' => true,
+                'password' => Hash::make('password'),
+                'email_verified_at' => now(),
+                'remember_token' => Str::random(10),
+                'phone' => '79991234567',
+                'avatar' => $faker->image('public/storage',640,480, null, false),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
     }
 }

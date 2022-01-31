5e15fc7d2646e4f07c6fa4031fffdf6e812af46b
kaminyv <kaminyv@yandex.ru>
2022-01-31 22:29:02 +0400

    Added middleware check user is master.

---
+++ app/Http/Middleware/CheckIsMaster.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CheckIsMaster
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse|JsonResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!Auth::user()['is_master']) {
+            return response()->json(['messege' => 'User not master.'], 422);
+        }
+        return $next($request);
+    }
+}
